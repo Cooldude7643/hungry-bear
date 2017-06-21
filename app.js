c4ed6abf8e7c2b58892fb1cc212a87e83651a590
Cooldude7643 <naturewilderness80845@gmail.com>
2017-06-20 14:58:20 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var arr = [false, undefined, "hello", 5, null];
+var animal = ["dog", "cat", "fish", "birds","bunny"]
+console.log(arr);
+console.log(animal);
+
+// var animal = "dog";
+// function runningAwayFromSomething(thing){
+//   if (thing === "dog"){
+//     console.log("Its ok, I love dogs");
+//   } else 
+//   console.log ("runningAwayFromSomething " + thing)
+// }
+// runningAwayFromSomething(animal);
